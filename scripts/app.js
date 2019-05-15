2e91dec56db03933416e313c8baa6493d1c662e1
Troy Dean <tald2@students.waikato.ac.nz>
2019-05-15 15:45:44 +1200

    validates city name

--- scripts/app.js
+++ scripts/app.js
@@ -12,20 +12,24 @@ $(document).ready(function () {
         accessToken: 'pk.eyJ1IjoiY29kZWNyYWZ0ZXJ0cm95IiwiYSI6ImNqdm04cGdxazBsbjk0YW50bzdwbXlxaGsifQ.xQjNqAnHi_3OjXC6rKhgHw'
     }).addTo(mymap);
 
-    $("#submit").click(function(){
+    $("#submit").click(function () {
         currentCity = $("#city").val();
-        if(currentCity.length == 0){
+        if (currentCity.length == 0) {
             alert("Please enter a city");
         } else {
             fetch('http://www.mapquestapi.com/geocoding/v1/address?key=SILNEFAPl42duMtaqi0GY7UHH6uxDa4M&location=' + currentCity + ',nz')
-                .then(function(response){
+                .then(function (response) {
                     return response.json();
-                }).then(function(cityDetails){
-                    var latLng = cityDetails.results[0].locations[0].latLng;
-                    mymap.setView([latLng.lat, latLng.lng], 13);
+                }).then(function (response) {
+                    var cityDeatils = response.results[0].locations[0];
+                    if (cityDeatils.adminArea5 == currentCity) {
+                        var latLng = cityDeatils.latLng;
+                        mymap.setView([latLng.lat, latLng.lng], 13);
+                    } else {
+                        alert(currentCity + " not found.\nPlease enter a valid city in New Zealand");
+                    }
                 });
-        }
-        console.log(currentCity);
+        }        
     });
 
 });
